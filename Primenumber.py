4a42b0e3ef3aff69c124cf45260026731d396e72
Denise <58773002+Denise-sys@users.noreply.github.com>
2021-01-25 14:30:48 +0100

    Create Primenumber.py
    Check if a number is a prime number. Simply method is: n is divisible by any integer between 2 and n/2 (inclusive).
    Ex. for i in range (2(n//2)+1)

---
+++ Primenumber.py
@@ -0,0 +1,16 @@
+
+def isPrime(n):
+  for i in range(2(2 // n) +1):
+  if n % i == o:
+    return False
+  return True
+  
+  
+def PrimeSequence(n):
+  s = '' 
+  for i in range (2, n + 1)
+      if isPrime(i):
+      s = s + str(i) + ', '
+      
+  return s
+  
