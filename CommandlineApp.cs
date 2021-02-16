45e36983aec0153b86e0e2063acced0ba01a1e4b
Denise <58773002+Denise-sys@users.noreply.github.com>
2021-02-16 21:55:12 +0100

    Update CommandlineApp.cs

--- CommandlineApp.cs
+++ CommandlineApp.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Calculator
+namespace Cinema
 {
     class Program
     {
@@ -14,20 +14,20 @@ namespace Calculator
             Console.WriteLine("\t4 - Documentaire, Blood Diamond");
             Console.Write("Your option? ");
 
-            // Use a switch statement to do the math.
+            // Use a switch statement to do the choosing.
             switch (Console.ReadLine())
             {
                 case "1":
-                    Console.WriteLine($"  ");
+                    Console.WriteLine($" Beschikbaar op; \n\n \t11 - aanstaande 28 maart \n \t12 - aanstaande 3 april \n \t13 - aanstaande 11 april \n ");
                     break;
                 case "2":
-                    Console.WriteLine($"  ");
+                    Console.WriteLine($" Beschikbaar op; \n\n \t14 - 19 maart \n \t15 - 2 april \n \t16 - 8 april \n  ");
                     break;
                 case "3":
-                    Console.WriteLine($"  " );
+                    Console.WriteLine($" Beschikbaar op; \n\n \t17 - 16 maart \n \t18 - 1 april \n \t19 - 6 april \n  ");
                     break;
                 case "4":
-                    Console.WriteLine($"  ");
+                    Console.WriteLine($" Beschikbaar op; \n\n \t20 - 26 maart \n \t21 - 8 april \n \t22 - 11 april \n  ");
                     break;
             }
             // Wait for the user to respond before closing.
